f29241e0db3fb3a3d0d86aba095bffc54474469a
Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>
2025-08-05 19:05:44 +0200

    feat: track `preloadCollection` queries
    Note: this may need further adjustment, as the preloading may cause many
    queries.
    
    Signed-off-by: Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>

--- apps/dav/lib/Connector/Sabre/Server.php
+++ apps/dav/lib/Connector/Sabre/Server.php
@@ -27,7 +27,8 @@ class Server extends \Sabre\DAV\Server {
 
 	/**
 	 * Tracks queries done by plugins.
-	 * @var array<int, array<string, array{nodes:int, queries:int}>>
+	 * @var array<string, array<int, array<string, array{nodes:int,
+	 *     queries:int}>>> The keys represent: event name, depth and plugin name
 	 */
 	private array $pluginQueries = [];
 
@@ -76,13 +77,16 @@ class Server extends \Sabre\DAV\Server {
 		callable $callBack,
 		int $priority = 100,
 	): void {
-		if ($eventName !== 'propFind') {
+		$pluginName = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)[2]['class'] ?? 'unknown';
+		// The NotifyPlugin needs to be excluded as it emits the
+		// `preloadCollection` event, which causes many plugins run queries.
+		if ($pluginName === PropFindPreloadNotifyPlugin::class || ($eventName !== 'propFind'
+				&& $eventName !== 'preloadCollection')) {
 			$parentFn($eventName, $callBack, $priority);
 			return;
 		}
 
-		$pluginName = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3)[2]['class'] ?? 'unknown';
-		$callback = $this->getMonitoredCallback($callBack, $pluginName);
+		$callback = $this->getMonitoredCallback($callBack, $pluginName, $eventName);
 
 		$parentFn($eventName, $callback, $priority);
 	}
@@ -94,22 +98,26 @@ class Server extends \Sabre\DAV\Server {
 	private function getMonitoredCallback(
 		callable $callBack,
 		string $pluginName,
+		string $eventName,
 	): callable {
 		return function (PropFind $propFind, INode $node) use (
 			$callBack,
 			$pluginName,
-		) {
+			$eventName,
+		): bool {
 			$connection = \OCP\Server::get(Connection::class);
 			$queriesBefore = $connection->getStats()['executed'];
 			$result = $callBack($propFind, $node);
 			$queriesAfter = $connection->getStats()['executed'];
 			$this->trackPluginQueries(
 				$pluginName,
+				$eventName,
 				$queriesAfter - $queriesBefore,
 				$propFind->getDepth()
 			);
 
-			return $result;
+			// many callbacks don't care about returning a bool
+			return $result ?? true;
 		};
 	}
 
@@ -118,6 +126,7 @@ class Server extends \Sabre\DAV\Server {
 	 */
 	private function trackPluginQueries(
 		string $pluginName,
+		string $eventName,
 		int $queriesExecuted,
 		int $depth,
 	): void {
@@ -126,11 +135,11 @@ class Server extends \Sabre\DAV\Server {
 			return;
 		}
 
-		$this->pluginQueries[$depth][$pluginName]['nodes']
-			= ($this->pluginQueries[$depth][$pluginName]['nodes'] ?? 0) + 1;
+		$this->pluginQueries[$eventName][$depth][$pluginName]['nodes']
+			= ($this->pluginQueries[$eventName][$depth][$pluginName]['nodes'] ?? 0) + 1;
 
-		$this->pluginQueries[$depth][$pluginName]['queries']
-			= ($this->pluginQueries[$depth][$pluginName]['queries'] ?? 0) + $queriesExecuted;
+		$this->pluginQueries[$eventName][$depth][$pluginName]['queries']
+			= ($this->pluginQueries[$eventName][$depth][$pluginName]['queries'] ?? 0) + $queriesExecuted;
 	}
 
 	/**
@@ -221,8 +230,8 @@ class Server extends \Sabre\DAV\Server {
 
 	/**
 	 * Returns queries executed by registered plugins.
-	 *
-	 * @return array<int, array<string, array{nodes:int, queries:int}>>
+	 * @return array<string, array<int, array<string, array{nodes:int,
+	 *     queries:int}>>> The keys represent: event name, depth and plugin name
 	 */
 	public function getPluginQueries(): array {
 		return $this->pluginQueries;
