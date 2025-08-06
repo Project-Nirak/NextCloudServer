cf6de98bea68fc94b11b9611f78da365bb35aab9
Anna Larch <anna@nextcloud.com>
2025-08-06 17:11:14 +0200

    fixup! fixup! feat(activity): add bulk activity option

--- lib/public/Activity/IBulkConsumer.php
+++ lib/public/Activity/IBulkConsumer.php
@@ -2,7 +2,7 @@
 
 /**
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 namespace OCP\Activity;
